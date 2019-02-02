6904dff8e5105d369ede665ff2a85c5b927d04f9
Jahan <zahanajani03@gmail.com>
2019-02-03 02:11:01 +0530

    added solution for Rotate Array

---
+++ Array/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> dict = new HashMap();
+        for(int i=0; i<nums.length; i++){
+            if(dict.get(nums[i]) != null )
+                return true;
+            dict.put(nums[i], 1);
+        }
+        return false;
+    }
+}
