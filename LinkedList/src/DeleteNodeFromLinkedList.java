aa0bd29272089e8cccf8bac5ac4caaab6df33774
Jahan <zahanajani03@gmail.com>
2019-02-14 02:18:45 +0530

    Added sol for DeleteNode

---
+++ LinkedList/src/DeleteNodeFromLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/553/
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+public class DeleteNodeFromLinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
