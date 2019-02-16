a82efdb7e414b5015e1f81a1f49fa4b2a5cef94f
Jahan <zahanajani03@gmail.com>
2019-02-16 22:57:28 +0530

    Added sol for isPalindromeLinkedList

---
+++ LinkedList/src/isPalindromeLinkedList.java
@@ -0,0 +1,38 @@
+public class isPalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode fast = head, temp, newhead = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            temp = head.next;
+            head.next = newhead;
+            newhead = head;
+            head = temp;
+
+
+        }
+        if (fast != null) {
+            head = head.next;
+        }
+        while (head != null) {
+            if (newhead.val != head.val) {
+                return false;
+            } else {
+                newhead = newhead.next;
+                head = head.next;
+            }
+        }
+        return true;
+
+    }
+
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
