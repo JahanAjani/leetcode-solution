0df16c5fcfda49f284d56ac6bf7294d286166f8e
Jahan <zahanajani03@gmail.com>
2019-02-20 20:39:54 +0530

    Added sol for ClimbingStairs

---
+++ DynamicProgramming/src/ClimbingStairs.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbingStairs {
+    Map<Integer, Integer> store = new HashMap();
+
+    public int climbStairsSolution(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        store.put(0, 1);
+        int cnt = countStairs(n);
+
+        return cnt;
+    }
+
+    public int countStairs(int n) {
+
+        if (store.containsKey(n)) {
+            return store.get(n);
+        }
+        int twos = 0, ones = 0;
+        if (n - 2 >= 0) {
+            twos = countStairs(n - 2);
+        }
+        if (n - 1 >= 0) {
+            ones = countStairs(n - 1);
+        }
+        store.put(n, twos + ones);
+        return twos + ones;
+    }
+}
