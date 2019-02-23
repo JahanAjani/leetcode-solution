a1d0af0aa33227af2b549f16fce32e726d2043a2
Jahan <zahanajani03@gmail.com>
2019-02-24 03:05:36 +0530

    Added sol for MaximumDepthOfTree

---
+++ Trees/src/MaximumDepthOfBT.java
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/94/trees/555/
+ */
+public class MaximumDepthOfBT {
+
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
